66ed83597f7d0c60ba0929a18fc23cf300411e4e
samirvithlani <samir.vithlani83955@gmail.com>
2023-12-11 19:22:23 +0530

    string

---
+++ string/string4.js
@@ -0,0 +1,7 @@
+var address = "201,surbhi complex,opp municipal market,c.g.road,ahmedabad-380009"
+
+var data = "hi this is india"
+
+var x = data.split(" ")
+console.log(x)
+console.log(x.length)
---
+++ string/string5.js
@@ -0,0 +1,20 @@
+
+
+var sname ="Dont plant bomb"
+var flag = sname.endsWith("om")
+console.log(flag) // true
+
+var ename ="bomb is hereb"
+var flag1 = ename.startsWith("b")
+console.log(flag1) // true
+
+
+var fname = "Hello"
+var ind = fname.search("Hl")
+console.log(ind) // 
+
+var lname = "hi i am narendra modi"
+var ind1 = lname.indexOf("i")
+var ind2 = lname.indexOf("i",5)
+console.log("index of..",ind1) // 4
+console.log("index of..",ind2) // 4
