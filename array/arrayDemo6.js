5d637b24581944a721dd2ab4aeddcd438d07b2d9
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-08 19:55:57 +0530

    array

---
+++ array/arrayDemo6.js
@@ -0,0 +1,13 @@
+const data = ["amit","raj","parth","raj","amit","raj","kunal"]
+
+var x  = data.join(",")
+
+console.log(data)
+console.log(x)
+
+
+var ind = data.indexOf("raj")
+console.log(ind)
+
+var ind1 = data.lastIndexOf("raj")
+console.log(ind1)
