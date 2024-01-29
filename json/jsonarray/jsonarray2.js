d438ef18970849996be13357da371434c414a8a4
samirvithlani <samir.vithlani83955@gmail.com>
2024-01-29 19:24:15 +0530

    json array

---
+++ json/jsonarray/jsonarray2.js
@@ -0,0 +1,69 @@
+var company = [
+  {
+    name: "Google",
+    employees: [
+      {
+        id: 101,
+        name: "Steve",
+        age: 32,
+        salary: 10000,
+        designation: "Developer",
+        gender: "male",
+        isMarried: true,
+        child: 2,
+      },
+      {
+        id: 102,
+        name: "Seeta",
+        age: 29,
+        salary: 1000,
+        designation: "Developer",
+        gender: "female",
+        isMarried: false,
+        child: 0,
+      },
+      {
+        id: 103,
+        name: "rima",
+        age: 36,
+        salary: 12000,
+        designation: "Developer",
+        gender: "female",
+        isMarried: true,
+        child: 1,
+      },
+    ],
+  },
+  {
+    name: "Microsoft",
+    employees: [
+      {
+        id: 101,
+        name: "bill",
+        age: 45,
+        salary: 90000,
+        designation: "Developer",
+        gender: "male",
+        isMarried: true,
+        child: 2,
+      },
+      {
+        id: 102,
+        name: "sita",
+        age: 29,
+        salary: 1000,
+        designation: "Developer",
+        gender: "female",
+        isMarried: false,
+        child: 0,
+      },
+    ],
+  },
+];
+
+//1. find all employees of google
+//2 find all female employee of microsoft
+//3) find all married employee of google
+//4) find all employee whose salary is greater than 10000
+//5) find all employee from microsoft who is having child
+//6)find female employee from google who is single
