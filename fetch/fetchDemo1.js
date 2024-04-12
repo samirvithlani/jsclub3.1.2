3a96587930dc587524111125c92744b82db6a7a0
samirvithlani <samir.vithlani83955@gmail.com>
2024-04-12 15:50:25 +0530

    get api

--- fetch/fetchDemo1.js
+++ fetch/fetchDemo1.js
@@ -1,12 +1,48 @@
-const getApiDemo1 = async () => {
+const getAPiDemo1 = async () => {
+
+        //1 - url
+        //2 - options
+        const res = await fetch("https://reqres.in/api/users?page=2",{
+                method:"GET",
+        })
+
+        const data = await res.json() //json() function returns a promise, and its resolved value is the JSON object deserialized from the response body.
+        console.log(res)
+        console.log("Data..",data)
+        console.log("Data..",data.data) //array
+        console.log(data.page)
+
+
+}
+
+
+const getAPiDemo2 = async () => {
+
+
+        const res = await fetch("https://dummy.restapiexample.com/api/v1/employees",{
+                method:"GET",
+        })
+        const data = await res.json()
+        console.log(res)
+        console.log("Data..",data)
+
+}
+
+//getAPiDemo1()
+//getAPiDemo2()
+const getAPiDemo3 = async () => {
 
 
 
-        fetch("")
-
-
-
+        const res = await fetch ("https://jsonplaceholder.typicode.com/posts",{
+                method:"GET",
+        })
+        console.log(res)
+        const data = await res.json()
+        console.log("Data..",data)
 
 
 
 }
+
+getAPiDemo3()
